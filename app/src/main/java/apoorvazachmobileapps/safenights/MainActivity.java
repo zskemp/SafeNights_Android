1b08a2e0ff150d37551bce9b838a0a6b66107d11
apoorva <aa3vs@virginia.edu>
2017-03-27 14:35:24 -0400

    added general workflow buttons

--- app/src/main/java/apoorvazachmobileapps/safenights/MainActivity.java
+++ app/src/main/java/apoorvazachmobileapps/safenights/MainActivity.java
@@ -1,7 +1,9 @@
 package apoorvazachmobileapps.safenights;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getStarted(View view) {
+        Intent intent = new Intent(this, GetStarted.class);
+        startActivity(intent);
+    }
+    public void addDrinks(View view) {
+        Intent intent = new Intent(this, AddDrinks.class);
+        startActivity(intent);
+    }
+    public void History(View view) {
+        Intent intent = new Intent(this, History.class);
+        startActivity(intent);
+    }
+    public void lastNight(View view) {
+        Intent intent = new Intent(this, LastNight.class);
+        startActivity(intent);
     }
 }
