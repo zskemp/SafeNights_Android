3c3434925aa2a4e94a3ca3b16f3a149a82b93f73
apoorva <aa3vs@virginia.edu>
2017-04-01 17:24:21 -0400

    updated some drink stuff

--- app/src/main/java/apoorvazachmobileapps/safenights/AddDrinks.java
+++ app/src/main/java/apoorvazachmobileapps/safenights/AddDrinks.java
@@ -6,9 +6,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddDrinks extends AppCompatActivity {
-
+    private SeekBar seekBar;
+    private DatePicker datePicker;
+    private TextView moneycount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +23,29 @@ public class AddDrinks extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        datePicker = (DatePicker)findViewById(R.id.datepicker);
+        seekBar = (SeekBar)findViewById(R.id.seekbar);
+        moneycount = (TextView)findViewById(R.id.moneycount);
+
+        moneycount.setText("Covered: " + seekBar.getProgress() + "/" + seekBar.getMax());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                moneycount.setText("Covered: " + progress + "/" + seekBar.getMax());
+            }
+        });
     }
 
 }
